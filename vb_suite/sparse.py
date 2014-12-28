58654509d0741ad2d03d8a7daf6c914575889c2b
David Cottrell <david.cottrell@gmail.com>
2014-12-28 19:52:10 +0000

    Add vbench tests. Have been unable to run vbench successfully so this is untested.

--- vb_suite/sparse.py
+++ vb_suite/sparse.py
@@ -37,3 +37,20 @@ stmt = "SparseDataFrame(columns=np.arange(100), index=np.arange(1000))"
 
 sparse_constructor = Benchmark(stmt, setup, name="sparse_frame_constructor",
                                start_date=datetime(2012, 6, 1))
+
+
+setup = common_setup + """
+s = pd.Series([3.0, nan, 1.0, 2.0, nan, nan])
+s.index = pd.MultiIndex.from_tuples([(1, 2, 'a', 0),
+                                     (1, 2, 'a', 1),
+                                     (1, 1, 'b', 0),
+                                     (1, 1, 'b', 1),
+                                     (2, 1, 'b', 0),
+                                     (2, 1, 'b', 1)])
+# SparseSeries
+ss = s.to_sparse()
+"""
+
+stmt = "ss.to_coo(ilevels=[0, 1], jlevels=[2, 3], sort_labels=True)"
+
+sparse_series_to_coo = Benchmark(stmt, setup, name="sparse_series_to_coo", start_date=datetime(2014, 12, 28))
